479c39c16e2145b979e7577967ce8325c2b65fe5
Diana Mayhugh <dianamayhugh@gmail.com>
2020-04-22 17:17:11 -0400

    counter +1 to clicks

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,14 @@
 //copied from crystal example for blueprint
 
+//counter used to track the user's total
+let counter = 0;
 // Here we created an on-click event that responds to button clicks of the crystal image.
 $("#blue").on("click", function() {
 
+  //each time the user clicks the blue crystal the counter goes up by 1.
+  counter += 1;
+
     // Clicking the button triggers an alert message.
-    alert("You clicked a crystal!");
+    alert("You clicked a crystal " + counter + " times!");
 
   });
