ad7df1ffbc53c2cecbe3197383eb39de63811cc8
Diana Mayhugh <dianamayhugh@gmail.com>
2020-04-23 16:30:52 -0400

    gems have random numbers that add to total points

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,13 +5,13 @@ $(document).ready(function () {
   let random = Math.floor(Math.random() * 101) + 19;
   $(".target-number").html(random);
 
- //player's wins and losses
+  //player's wins and losses
   let wins = 0;
   let losses = 0;
 
   //points player has during the game
   let points = 0;
-  $('.points').html(points);
+  $(".points").html(points);
 
   //and array with numbers 1-12 for each crystal to pick a random number from
 
@@ -23,36 +23,22 @@ $(document).ready(function () {
 
   // Here we created an on-click event that responds to button clicks of the crystal image.
   $("#blue").on("click", function () {
-    //each time the user clicks the blue crystal the counter goes up by 1.
-    blue 
-
-    // Clicking the button triggers an alert message.
-    alert("Your new score is " + blue);
+    points = points + blue;
+    $(".points").html(points)
   });
 
   $("#red").on("click", function () {
-    //each time the user clicks the blue crystal the counter goes up by 1.
-    red 
-
-    // Clicking the button triggers an alert message.
-    alert("Your new score is " + red);
+    points = points + red;
+    $(".points").html(points)
   });
 
   $("#green").on("click", function () {
-    //each time the user clicks the blue crystal the counter goes up by 1.
-    green
-
-    // Clicking the button triggers an alert message.
-    alert("Your new score is " + green);
+    points = points + green;
+    $(".points").html(points)
   });
 
   $("#yellow").on("click", function () {
-    //each time the user clicks the blue crystal the counter goes up by 1.
-    blue ++
-
-    // Clicking the button triggers an alert message.
-    alert("Your new score is " + yellow);
+    points = points + yellow;
+    $(".points").html(points)
   });
-
-  
 });
