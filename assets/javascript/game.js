26be8d775ec098c0708c18f395adf60f365b1041
Diana Mayhugh <dianamayhugh@gmail.com>
2020-04-23 15:51:43 -0400

    target number and points

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,27 +1,61 @@
 //used crystal example and lottery generator for blueprint
 
 $(document).ready(function () {
-  //counter used to track the user's total
-  let counter = 0;
+  // target random number between 19 -120
+  let random = Math.floor(Math.random() * 101) + 19;
+  $(".target-number").html(random);
 
-  var randomGem = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-  var increment = randomGem[Math.round(Math.random())];
+ //player's wins and losses
+  let wins = 0;
+  let losses = 0;
 
-  // Here we created an on-click event that responds to button clicks of the crystal image.
-  $("#blue").on("click", function () {
-    //each time the user clicks the blue crystal the counter goes up by 1.
-    counter += increment;
+  //points player has during the game
+  let points = 0;
+  $('.points').html(points);
+  
 
-    // Clicking the button triggers an alert message.
-    alert("Your new score is " + counter);
-  });
+  // var randomGem = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+  // var increment = randomGem[Math.round(Math.random())];
 
-  $(".target-number").ready(function () {
-    // Create a string which will hold the target or starting number
-    var targetNumber = "";
+  // // Here we created an on-click event that responds to button clicks of the crystal image.
+  // $("#blue").on("click", function () {
+  //   //each time the user clicks the blue crystal the counter goes up by 1.
+  //   counter ++ increment;
 
-    var random = Math.floor(Math.random() * 101) + 19;
+  //   // Clicking the button triggers an alert message.
+  //   alert("Your new score is " + counter);
+  // });
 
-    $(".target-number").html(random);
-  });
+  // $("#red").on("click", function () {
+  //   //each time the user clicks the blue crystal the counter goes up by 1.
+  //   counter += increment;
+
+  //   // Clicking the button triggers an alert message.
+  //   alert("Your new score is " + counter);
+  // });
+
+  // $("#green").on("click", function () {
+  //   //each time the user clicks the blue crystal the counter goes up by 1.
+  //   counter += increment;
+
+  //   // Clicking the button triggers an alert message.
+  //   alert("Your new score is " + counter);
+  // });
+
+  // $("#yellow").on("click", function () {
+  //   //each time the user clicks the blue crystal the counter goes up by 1.
+  //   counter += increment;
+
+  //   // Clicking the button triggers an alert message.
+  //   alert("Your new score is " + counter);
+  // });
+
+  // $(".target-number").ready(function () {
+  //   // Create a string which will hold the target or starting number
+  //   var targetNumber = "";
+
+  //   var random = Math.floor(Math.random() * 101) + 19;
+
+  //   $(".target-number").html(random);
+  // });
 });
