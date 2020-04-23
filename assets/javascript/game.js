02a699d82d60122e03e9de5dfa73ade4dd6015ac
Diana Mayhugh <dianamayhugh@gmail.com>
2020-04-22 22:47:40 -0400

    blue gem starts random number and increments

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,40 +1,27 @@
 //used crystal example and lottery generator for blueprint
 
-$(document).ready(function() {
-//counter used to track the user's total
-let counter = 0;
-// Here we created an on-click event that responds to button clicks of the crystal image.
-$("#blue").on("click", function() {
+$(document).ready(function () {
+  //counter used to track the user's total
+  let counter = 0;
 
-  //each time the user clicks the blue crystal the counter goes up by 1.
-  counter += 10;
+  var randomGem = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+  var increment = randomGem[Math.round(Math.random())];
+
+  // Here we created an on-click event that responds to button clicks of the crystal image.
+  $("#blue").on("click", function () {
+    //each time the user clicks the blue crystal the counter goes up by 1.
+    counter += increment;
 
     // Clicking the button triggers an alert message.
     alert("Your new score is " + counter);
-
   });
 
-
-  $(".target-number").on("click", function() {
-
-    // Create a string which will hold the lottery number
+  $(".target-number").ready(function () {
+    // Create a string which will hold the target or starting number
     var targetNumber = "";
 
     var random = Math.floor(Math.random() * 101) + 19;
 
-    $(".target-number").text(random);
-
-    });
-
+    $(".target-number").html(random);
   });
-
-
-
-
-
-
-
-
-
-
-
+});
