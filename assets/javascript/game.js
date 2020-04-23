64ef6adcc5d38c231d640e432a4a7485f4e18794
Diana Mayhugh <dianamayhugh@gmail.com>
2020-04-23 16:08:27 -0400

    each gem produces a random number

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,50 +12,47 @@ $(document).ready(function () {
   //points player has during the game
   let points = 0;
   $('.points').html(points);
+
+  //and array with numbers 1-12 for each crystal to pick a random number from
+
+  let randomGem = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+  let blue = randomGem[Math.floor(Math.random() * randomGem.length)];
+  let red = randomGem[Math.floor(Math.random() * randomGem.length)];
+  let green = randomGem[Math.floor(Math.random() * randomGem.length)];
+  let yellow = randomGem[Math.floor(Math.random() * randomGem.length)];
+
+  // Here we created an on-click event that responds to button clicks of the crystal image.
+  $("#blue").on("click", function () {
+    //each time the user clicks the blue crystal the counter goes up by 1.
+    blue 
+
+    // Clicking the button triggers an alert message.
+    alert("Your new score is " + blue);
+  });
+
+  $("#red").on("click", function () {
+    //each time the user clicks the blue crystal the counter goes up by 1.
+    red 
+
+    // Clicking the button triggers an alert message.
+    alert("Your new score is " + red);
+  });
+
+  $("#green").on("click", function () {
+    //each time the user clicks the blue crystal the counter goes up by 1.
+    green
+
+    // Clicking the button triggers an alert message.
+    alert("Your new score is " + green);
+  });
+
+  $("#yellow").on("click", function () {
+    //each time the user clicks the blue crystal the counter goes up by 1.
+    blue ++
+
+    // Clicking the button triggers an alert message.
+    alert("Your new score is " + yellow);
+  });
+
   
-
-  // var randomGem = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-  // var increment = randomGem[Math.round(Math.random())];
-
-  // // Here we created an on-click event that responds to button clicks of the crystal image.
-  // $("#blue").on("click", function () {
-  //   //each time the user clicks the blue crystal the counter goes up by 1.
-  //   counter ++ increment;
-
-  //   // Clicking the button triggers an alert message.
-  //   alert("Your new score is " + counter);
-  // });
-
-  // $("#red").on("click", function () {
-  //   //each time the user clicks the blue crystal the counter goes up by 1.
-  //   counter += increment;
-
-  //   // Clicking the button triggers an alert message.
-  //   alert("Your new score is " + counter);
-  // });
-
-  // $("#green").on("click", function () {
-  //   //each time the user clicks the blue crystal the counter goes up by 1.
-  //   counter += increment;
-
-  //   // Clicking the button triggers an alert message.
-  //   alert("Your new score is " + counter);
-  // });
-
-  // $("#yellow").on("click", function () {
-  //   //each time the user clicks the blue crystal the counter goes up by 1.
-  //   counter += increment;
-
-  //   // Clicking the button triggers an alert message.
-  //   alert("Your new score is " + counter);
-  // });
-
-  // $(".target-number").ready(function () {
-  //   // Create a string which will hold the target or starting number
-  //   var targetNumber = "";
-
-  //   var random = Math.floor(Math.random() * 101) + 19;
-
-  //   $(".target-number").html(random);
-  // });
 });
